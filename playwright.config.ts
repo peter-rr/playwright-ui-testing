07ab0c8679bf05a57628b7611eda48b378eacf6d
Pedro Ramos <pedroramosromero@gmail.com>
2024-12-31 15:16:24 +0100

    Add some 'trace' property

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,6 +30,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    // trace: 'on' -> always creates a trace for every test
   },
 
   /* Configure projects for major browsers */
